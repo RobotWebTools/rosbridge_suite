58739867499b97e778af0080d2d8f87ac99d9512
jon <jon@msi>
2012-08-16 14:42:46 -0700

    Added workaround to a rospy bug that doesnt handle multithreaded rosparam requests well.

--- rosbridge/rosapi/src/params.py
+++ rosbridge/rosapi/src/params.py
@@ -1,6 +1,7 @@
 import rospy
 
 from json import loads, dumps
+from httplib import CannotSendRequest, ResponseNotReady
 
 
 """ Methods to interact with the param server.  Values have to be passed
@@ -23,8 +24,15 @@ def get_param(name, default):
             d = loads(default)
         except ValueError:
             d = default
-    return dumps(rospy.get_param(name, d))
+    
+    return dumps(get_param_obsessively(name, d))
 
+def get_param_obsessively(name, d):
+    # Workaround to a rospy bug
+    try:
+        return rospy.get_param(name, d)
+    except CannotSendRequest, ResponseNotReady:
+        return get_param_obsessively(name, d)
 
 def has_param(name):
     return rospy.has_param(name)
