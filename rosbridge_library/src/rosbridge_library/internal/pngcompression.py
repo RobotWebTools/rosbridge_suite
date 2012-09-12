525a58da93ad60b5c778a3471d272b601cc37464
Russell Toris <rctoris@wpu.edu>
2012-09-12 11:20:23 -0400

    PNG compression now creates a square RGB image padded with new-line characters

--- rosbridge_library/src/rosbridge_library/internal/pngcompression.py
+++ rosbridge_library/src/rosbridge_library/internal/pngcompression.py
@@ -1,11 +1,19 @@
 from PIL import Image
 from base64 import standard_b64encode, standard_b64decode
 from StringIO import StringIO
+from math import floor, ceil, sqrt
 
 
 def encode(string):
-    """ PNG-compress the string, return the b64 encoded bytes """
-    i = Image.fromstring('L', (len(string), 1), string)
+    """ PNG-compress the string in a square RBG image padded with '\n', return the b64 encoded bytes """
+    length = len(string)
+    width = floor(sqrt(length/3.0))
+    height = ceil((length/3.0) / width)
+    bytes_needed = int(width * height * 3)
+    while length < bytes_needed:
+        string += '\n'
+        length += 1
+    i = Image.fromstring('RGB', (int(width), int(height)), string)
     buff = StringIO()
     i.save(buff, "png")
     encoded = standard_b64encode(buff.getvalue())
