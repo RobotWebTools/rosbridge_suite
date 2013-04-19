427d7aa5b365badb8204877e0116a990764ba09f
Brandon Alexander <baalexander@gmail.com>
2013-04-18 17:49:30 -0700

    Removes trailing commas.

--- rosapi/setup.py
+++ rosapi/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['rosapi'],
-    package_dir={'': 'src'},
+    package_dir={'': 'src'}
 )
 
 setup(**d)
--- rosbridge_server/setup.py
+++ rosbridge_server/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['tornado', 'tornado.platform'],
-    package_dir={'': 'src'},
+    package_dir={'': 'src'}
 )
 
 setup(**d)
