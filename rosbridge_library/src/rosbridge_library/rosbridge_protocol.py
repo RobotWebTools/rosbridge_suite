4082a371dce62d24ca0c59299e75ac83022ef3dc
fxm-db <fxm-db@makalu-fxm.(none)>
2013-06-04 19:25:25 +0200

    add defragmentation capability

--- rosbridge_library/src/rosbridge_library/rosbridge_protocol.py
+++ rosbridge_library/src/rosbridge_library/rosbridge_protocol.py
@@ -3,12 +3,20 @@ from rosbridge_library.capabilities.call_service import CallService
 from rosbridge_library.capabilities.advertise import Advertise
 from rosbridge_library.capabilities.publish import Publish
 from rosbridge_library.capabilities.subscribe import Subscribe
+# import for defragmentation
+from rosbridge_library.capabilities.defragmentation import Defragment
+
+# uncomment to enable Fragmentation
+#from rosbridge_library.capabilities.fragmentation import Fragmentation
 
 
 class RosbridgeProtocol(Protocol):
     """ Adds the handlers for the rosbridge opcodes """
+    rosbridge_capabilities = [CallService, Advertise, Publish, Subscribe, Defragment]
 
-    rosbridge_capabilities = [CallService, Advertise, Publish, Subscribe]
+    print "registered capabilities:"
+    for cap in rosbridge_capabilities:
+        print "-", str(cap)
 
     def __init__(self, client_id):
         Protocol.__init__(self, client_id)
