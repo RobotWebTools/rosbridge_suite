b673351f57abb6973e39363b895cd065b7e78a53
Stefan Profanter <git@s.profanter.me>
2013-11-04 14:34:44 +0100

    Merged hydro-devel into fuerte-devel (src only)

---
+++ rosbridge_library/CHANGELOG.rst
@@ -0,0 +1,7 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package rosbridge_library
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.5.1 (2013-11-04)
+------------------
+Merged hydro-devel src directory into fuerte-devel
--- rosbridge_library/src/rosbridge_library/protocol.py
+++ rosbridge_library/src/rosbridge_library/protocol.py
@@ -1,19 +1,44 @@
-from rospy import logdebug, loginfo, logwarn, logerr
-try:
-    from ujson import dumps, loads
-except ImportError:
-    try:
-        from simplejson import dumps, loads
-    except ImportError:
-        from json import dumps, loads
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+import json
+
 
 class InvalidArgumentException(Exception):
     pass
 
-
 class MissingArgumentException(Exception):
     pass
-
 
 class Protocol:
     """ The interface for a single client to interact with ROS.
@@ -118,7 +143,7 @@ class Protocol:
         Returns a JSON string representing the dictionary
         """
         try:
-            return dumps(msg)
+            return json.dumps(msg)
         except:
             if cid is not None:
                 # Only bother sending the log message if there's an id
@@ -139,7 +164,7 @@ class Protocol:
 
         """
         try:
-            return loads(msg)
+            return json.loads(msg)
         except:
             self.log("error",
              "Unable to deserialize message from client: %s" % msg, cid)
@@ -193,10 +218,11 @@ class Protocol:
             stdout_formatted_msg = "[Client %s] %s" % (self.client_id, message)
 
         if level == "error" or level == "err":
-            logerr(stdout_formatted_msg)
+            rospy.logerr(stdout_formatted_msg)
         elif level == "warning" or level == "warn":
-            logwarn(stdout_formatted_msg)
+            rospy.logwarn(stdout_formatted_msg)
         elif level == "info" or level == "information":
-            loginfo(stdout_formatted_msg)
+            rospy.loginfo(stdout_formatted_msg)
         else:
-            logdebug(stdout_formatted_msg)
+            rospy.logdebug(stdout_formatted_msg)
+
