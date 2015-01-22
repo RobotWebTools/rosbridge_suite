97e43cf06c608b098fa44875f80812e0d968fad2
Jihoon Lee <jihoonlee.in@gmail.com>
2015-01-22 13:44:34 +0900

    don't unregister topic from rosbridge. It creates md5 sum warning.. #138

--- rosbridge_library/src/rosbridge_library/internal/publishers.py
+++ rosbridge_library/src/rosbridge_library/internal/publishers.py
@@ -308,9 +308,11 @@ class PublisherManager():
 
         self._publishers[topic].unregister_client(client_id)
 
+        """
         if not self._publishers[topic].has_clients():
             self._publishers[topic].unregister()
             del self._publishers[topic]
+        """
 
     def unregister_all(self, client_id):
         """ Unregisters a client from all publishers that they are registered
