66ba6132e3a935220ffbbb0295e9030ce963f157
donghoonpark <donghoonpark@github.com>
2016-07-12 13:30:47 +0900

    client working

---
+++ rosbridge_client/src/rosbridge_client/client.py
@@ -0,0 +1,13 @@
+import rospy
+from rosbridge_library.protocol import Protocol
+from std_msgs.msg import Float32
+from sensor_msgs.msg import Com
+
+def publish(msg):
+
+
+
+def subscribe(topic, callback):
+
+
+class Rosbridge_client:
