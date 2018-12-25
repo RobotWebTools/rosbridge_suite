6c93331fa7515c85f2f9e2bf91ef8eb05f9d7828
David Weis <davidmichaelweis@gmail.com>
2018-12-25 08:38:55 +0000

    Fix typo (#379)

--- rosbridge_library/src/rosbridge_library/util/__init__.py
+++ rosbridge_library/src/rosbridge_library/util/__init__.py
@@ -1,4 +1,4 @@
-# try to import json-lib: 1st try usjon, 2nd try simplejson, else import standard python json
+# try to import json-lib: 1st try ujson, 2nd try simplejson, else import standard python json
 try:
     import ujson as json
 except ImportError:
