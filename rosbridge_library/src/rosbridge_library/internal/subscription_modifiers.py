96186aea7f3706207903c11ac656d9104759c7db
Matt Vollrath <matt@endpoint.com>
2020-05-12 16:01:02 -0400

    Don't add messages to finished queue handler

--- rosbridge_library/src/rosbridge_library/internal/subscription_modifiers.py
+++ rosbridge_library/src/rosbridge_library/internal/subscription_modifiers.py
@@ -115,6 +115,9 @@ class QueueMessageHandler(MessageHandler, Thread):
 
     def handle_message(self, msg):
         with self.c:
+            if not self.alive:
+                return
+
             should_notify = len(self.queue) == 0
             self.queue.append(msg)
             if should_notify:
