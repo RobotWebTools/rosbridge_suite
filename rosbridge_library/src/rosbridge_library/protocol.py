dfc7ca88c97b092352c342dede8592680cdd0da4
Dmitri <dmitri@dmitri.com>
2020-07-07 21:37:06 -0400

    possible fix for error when working with RosSharp, TypeError: can only concatenate str (not bytes) to str

--- rosbridge_library/src/rosbridge_library/protocol.py
+++ rosbridge_library/src/rosbridge_library/protocol.py
@@ -117,7 +117,7 @@ class Protocol:
         message_string -- the wire-level message sent by the client
 
         """
-        self.buffer = self.buffer + message_string
+        self.buffer = self.buffer + str(message_string)
         msg = None
 
         # take care of having multiple JSON-objects in receiving buffer
