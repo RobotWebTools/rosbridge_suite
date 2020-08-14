1b875df29404f92bf588fc7f6140ab5a063b3107
Travis Prosser <travisprosser@gmail.com>
2020-08-14 14:25:04 -0300

    fix: remove json encoding before setting string params

--- rosapi/src/rosapi/params.py
+++ rosapi/src/rosapi/params.py
@@ -78,6 +78,7 @@ def set_param(node_name, name, value, params_glob):
     d = None
     try:
         d = loads(value)
+        value = d if isinstance(d, str) else value
     except ValueError:
         raise Exception("Due to the type flexibility of the ROS parameter server, the value argument to set_param must be a JSON-formatted string.")
 
