be5d4beeab4e7e420315e1b5dc8b430f960e430a
Ivor Wanders <ivor@iwanders.net>
2021-06-15 14:09:37 -0400

    Prevent empty topics_globs that allow everything.
    If the input `topics_glob` isn't empty, always ensure that all the returns
    contain at least one element to prevent the individual capabilities from
    assuming that everything is allowed.

--- rosbridge_server/src/rosbridge_server/util.py
+++ rosbridge_server/src/rosbridge_server/util.py
@@ -21,6 +21,14 @@ def directional_topics_globs(topics_glob):
         By default topic communication is allowed in both directions.
         Topics prefixed with `>` are only allowed in outgoing (to the websocket).
         Topics prefixed with `<` are only allowed in ingoing (from the websocket).
+
+        If the input topics_glob is empty, it returns empty lists and everything
+        is allowed.
+
+        If the input topics_glob is not empty, it will ensure that the output
+        topics_globs contain at least one element (an empty string) that will
+        ensure that capabilities don't assume everything is allowed because the
+        topics_glob list was empty.
     """
     incoming = set()
     outgoing = set()
@@ -30,14 +38,25 @@ def directional_topics_globs(topics_glob):
             topic_path = t[1:]
             outgoing.add(topic_path)
         elif t.startswith("<"):
-            # Allow publishing from the websocket only, not subscribing to the messages.
+            # Allow publishing from the websocket only, not subscribing.
             topic_path = t[1:]
             incoming.add(topic_path)
         else:
             # Allow both subscription and advertise / subscribe.
             incoming.add(t)
             outgoing.add(t)
-    return {"incoming": list(incoming),
-            "outgoing": list(outgoing),
-            "bidirectional": list(incoming & outgoing),
-            "all": list(incoming | outgoing)}
+
+    # The topics_globs assume that empty lists means allow anything. So we
+    # need to insert a dummy value to prevent the list from being empty
+    # while at the same time preserving backwards compatilibity in case
+    # the input topics_glob is really empty and we want to allow everything.
+    if len(topics_glob) == 0:
+        return {"incoming": [], "outgoing": [], "bidirectional": [], "all": []}
+
+    # Some topic was specified, in this case none of the returns may be empty lists.
+    # we insert `""`, this can only ever match the empty string itself, which is an
+    # invalid topic name.
+    return {"incoming": list(incoming | set([""])),
+            "outgoing": list(outgoing | set([""])),
+            "bidirectional": list((incoming & outgoing) | set([""])),
+            "all": list(incoming | outgoing | set([""]))}
