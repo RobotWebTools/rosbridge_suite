ac94d9b15c6ac5a074cfd2388d29e7fec1ee1219
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2021-08-12 15:49:53 -0700

    Remove authentication features and rosauth dependency (#586)
    [rosauth](http://wiki.ros.org/rosauth) is not maintained for ROS 2, and has not been released for Galactic (https://github.com/GT-RAIL/rosauth/issues/35). Since the authentication feature is old and not commonly used, and since rosbridge_suite has not yet been released in Eloquent, Foxy, or Galactic, we decided to just remove the authentication features to unblock us from pushing releases.
    
    To avoid breaking backwards compatibility, we will not publish the new version of rosbridge_suite for Dashing.

--- rosbridge_server/src/rosbridge_server/udp_handler.py
+++ rosbridge_server/src/rosbridge_server/udp_handler.py
@@ -1,5 +1,4 @@
 import rospy
-from rosauth.srv import Authentication
 from rosbridge_library.rosbridge_protocol import RosbridgeProtocol
 from rosbridge_library.util import json, bson
 
@@ -23,7 +22,6 @@ class RosbridgeUdpSocket:
     client_id_seed = 0
     clients_connected = 0
     client_count_pub = None
-    authenticate = False
 
     # The following parameters are passed on to RosbridgeProtocol
     # defragmentation.py:
@@ -35,7 +33,6 @@ class RosbridgeUdpSocket:
 
     def __init__(self,write):
         self.write = write
-        self.authenticated = False
 
     def startProtocol(self):
         cls = self.__class__
@@ -48,7 +45,6 @@ class RosbridgeUdpSocket:
         try:
             self.protocol = RosbridgeProtocol(cls.client_id_seed, parameters=parameters)
             self.protocol.outgoing = self.send_message
-            self.authenticated = False
             cls.client_id_seed += 1
             cls.clients_connected += 1
             if cls.client_count_pub:
@@ -56,35 +52,9 @@ class RosbridgeUdpSocket:
         except Exception as exc:
             rospy.logerr("Unable to accept incoming connection.  Reason: %s", str(exc))
         rospy.loginfo("Client connected.  %d clients total.", cls.clients_connected)
-        if cls.authenticate:
-            rospy.loginfo("Awaiting proper authentication...")
 
     def datagramReceived(self, message):
-        cls = self.__class__
-        # check if we need to authenticate
-        if cls.authenticate and not self.authenticated:
-            try:
-                msg = json.loads(message)
-                if msg['op'] == 'auth':
-                    # check the authorization information
-                    auth_srv = rospy.ServiceProxy('authenticate', Authentication)
-                    resp = auth_srv(msg['mac'], msg['client'], msg['dest'],
-                                                  msg['rand'], rospy.Time(msg['t']), msg['level'],
-                                                  rospy.Time(msg['end']))
-                    self.authenticated = resp.authenticated
-                    if self.authenticated:
-                        rospy.loginfo("Client %d has authenticated.", self.protocol.client_id)
-                        return
-                # if we are here, no valid authentication was given
-                rospy.logwarn("Client %d did not authenticate. Closing connection.",
-                              self.protocol.client_id)
-                self.close()
-            except:
-                # proper error will be handled in the protocol class
-                self.protocol.incoming(message)
-        else:
-            # no authentication required
-            self.protocol.incoming(message)
+        self.protocol.incoming(message)
 
     def stopProtocol(self):
         cls = self.__class__
