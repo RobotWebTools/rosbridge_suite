e377f37eadd1a41519923339aaea101b858f43b6
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-11-29 15:17:09 -0800

    Delete unsupported TCP and UDP server implementations in ROS 2 branch (#685)
    **Public API Changes**
    None; TCP and UDP servers never worked in ROS 2.
    
    
    **Description**
    Closes https://github.com/RobotWebTools/rosbridge_suite/issues/639

--- rosbridge_server/src/rosbridge_server/__init__.py
+++ rosbridge_server/src/rosbridge_server/__init__.py
@@ -1,6 +1,2 @@
 from .client_mananger import ClientManager  # noqa: F401
 from .websocket_handler import RosbridgeWebSocket  # noqa: F401
-
-# TODO(@jubeira): add imports again once the modules are ported to ROS2.
-# from .tcp_handler import RosbridgeTcpSocket
-# from .udp_handler import RosbridgeUdpSocket,RosbridgeUdpFactory
