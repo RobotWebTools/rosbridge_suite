cbea3a9895ca52722697931674e58e14356304fc
kedus42 <k.mathewos@jacobs-university.de>
2022-02-06 13:38:03 +0100

    CBOR value extraction expanded to include messages with no slot nd slot type attribute

--- rosbridge_library/src/rosbridge_library/internal/cbor_conversion.py
+++ rosbridge_library/src/rosbridge_library/internal/cbor_conversion.py
@@ -50,53 +50,103 @@ def extract_cbor_values(msg):
     Typed arrays will be tagged and packed into byte arrays.
     """
     out = {}
-    for slot, slot_type in zip(msg.__slots__, msg._slot_types):
-        val = getattr(msg, slot)
+    if hasattr(msg, "get_fields_and_field_types"):
+        for slot, slot_type in msg.get_fields_and_field_types().items():
+            val = getattr(msg, slot)
 
-        # string
-        if slot_type in STRING_TYPES:
-            out[slot] = str(val)
+            # string
+            if slot_type in STRING_TYPES:
+                out[slot] = str(val)
 
-        # bool
-        elif slot_type in BOOL_TYPES:
-            out[slot] = bool(val)
+            # bool
+            elif slot_type in BOOL_TYPES:
+                out[slot] = bool(val)
 
-        # integers
-        elif slot_type in INT_TYPES:
-            out[slot] = int(val)
+            # integers
+            elif slot_type in INT_TYPES:
+                out[slot] = int(val)
 
-        # floats
-        elif slot_type in FLOAT_TYPES:
-            out[slot] = float(val)
+            # floats
+            elif slot_type in FLOAT_TYPES:
+                out[slot] = float(val)
 
-        # time/duration
-        elif slot_type in TIME_TYPES:
-            out[slot] = {
-                "secs": int(val.secs),
-                "nsecs": int(val.nsecs),
-            }
+            # time/duration
+            elif slot_type in TIME_TYPES:
+                out[slot] = {
+                    "secs": int(val.secs),
+                    "nsecs": int(val.nsecs),
+                }
 
-        # byte array
-        elif slot_type in BYTESTREAM_TYPES:
-            out[slot] = bytes(val)
+            # byte array
+            elif slot_type in BYTESTREAM_TYPES:
+                out[slot] = bytes(val)
 
-        # bool array
-        elif slot_type in BOOL_ARRAY_TYPES:
-            out[slot] = [bool(i) for i in val]
+            # bool array
+            elif slot_type in BOOL_ARRAY_TYPES:
+                out[slot] = [bool(i) for i in val]
 
-        # numeric arrays
-        elif slot_type in TAGGED_ARRAY_FORMATS:
-            tag, fmt = TAGGED_ARRAY_FORMATS[slot_type]
-            fmt_to_length = fmt.format(len(val))
-            packed = struct.pack(fmt_to_length, *val)
-            out[slot] = Tag(tag=tag, value=packed)
+            # numeric arrays
+            elif slot_type in TAGGED_ARRAY_FORMATS:
+                tag, fmt = TAGGED_ARRAY_FORMATS[slot_type]
+                fmt_to_length = fmt.format(len(val))
+                packed = struct.pack(fmt_to_length, *val)
+                out[slot] = Tag(tag=tag, value=packed)
 
-        # array of messages
-        elif type(val) in LIST_TYPES:
-            out[slot] = [extract_cbor_values(i) for i in val]
+            # array of messages
+            elif type(val) in LIST_TYPES:
+                out[slot] = [extract_cbor_values(i) for i in val]
 
-        # message
-        else:
-            out[slot] = extract_cbor_values(val)
+            # message
+            else:
+                out[slot] = extract_cbor_values(val)
+        return out
+    else:
+        for slot, slot_type in zip(msg.__slots__, msg._slot_types):
+            val = getattr(msg, slot)
 
-    return out
+            # string
+            if slot_type in STRING_TYPES:
+                out[slot] = str(val)
+
+            # bool
+            elif slot_type in BOOL_TYPES:
+                out[slot] = bool(val)
+
+            # integers
+            elif slot_type in INT_TYPES:
+                out[slot] = int(val)
+
+            # floats
+            elif slot_type in FLOAT_TYPES:
+                out[slot] = float(val)
+
+            # time/duration
+            elif slot_type in TIME_TYPES:
+                out[slot] = {
+                    "secs": int(val.secs),
+                    "nsecs": int(val.nsecs),
+                }
+
+            # byte array
+            elif slot_type in BYTESTREAM_TYPES:
+                out[slot] = bytes(val)
+
+            # bool array
+            elif slot_type in BOOL_ARRAY_TYPES:
+                out[slot] = [bool(i) for i in val]
+
+            # numeric arrays
+            elif slot_type in TAGGED_ARRAY_FORMATS:
+                tag, fmt = TAGGED_ARRAY_FORMATS[slot_type]
+                fmt_to_length = fmt.format(len(val))
+                packed = struct.pack(fmt_to_length, *val)
+                out[slot] = Tag(tag=tag, value=packed)
+
+            # array of messages
+            elif type(val) in LIST_TYPES:
+                out[slot] = [extract_cbor_values(i) for i in val]
+
+            # message
+            else:
+                out[slot] = extract_cbor_values(val)
+        return out
