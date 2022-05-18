274be59990260a525bc2fc758e6f0c4d142bf27f
William Wedler <wwedler@riseup.net>
2022-05-18 01:30:40 -0400

    run test that has exception and traceback

--- rosbridge_server/test/websocket/rapid_disconnect_smoke.test.py
+++ rosbridge_server/test/websocket/rapid_disconnect_smoke.test.py
@@ -2,11 +2,14 @@
 import asyncio
 import os
 import sys
+import math
 import unittest
 
+import rclpy.task
 from rclpy.node import Node
 from std_msgs.msg import String
 from twisted.python import log
+from typing import Any, Awaitable, Callable
 
 sys.path.append(os.path.dirname(__file__))  # enable importing from common.py in this directory
 
@@ -17,22 +20,26 @@ log.startLogging(sys.stderr)
 
 generate_test_description = common.generate_test_description
 
-
 class TestWebsocketSmoke(unittest.TestCase):
+    def sub_many(self, node: Node, duration: float, topic: str, sub_ws_clients, publisher, content) -> Awaitable[None]:
+        """
+        Async-compatible delay function based on a ROS timer.
+        """
+        future = rclpy.task.Future()
+        self.count = 0
 
-    async def pub_many_times(publisher, content):
-        for _ in range(1000):
-            publisher.publish(String(data=content))
-        return 0
-
-
-    async def sub_many_times(topic, make_client):
-        for _ in range(10):
-            # Make several subscibers that will subscribe and then immediately disconnect
-            sub_ws_clients = []
-            for _ in range(100):
-                ws_client = await make_client()
-                ws_client.sendJson(
+        node.get_logger().info('sub many')
+        def callback():
+            node.get_logger().info('{}'.format(self.count))
+            self.count = self.count + 1
+            if self.count >= len(sub_ws_clients) * 2:
+                future.set_result(None)
+                timer.cancel()
+                pub_timer.cancel()
+                node.destroy_timer(timer)
+                node.destroy_timer(pub_timer)
+            elif self.count < len(sub_ws_clients):
+                sub_ws_clients[self.count].sendJson(
                     {
                         "op": "subscribe",
                         "topic": topic,
@@ -40,17 +47,22 @@ class TestWebsocketSmoke(unittest.TestCase):
                         "queue_length": 0,  # Test the roslib default.
                     }
                 )
-                sub_ws_clients.append(ws_client)
-            for ws_client in sub_ws_clients:
+            else:
                 #client.sendClose()
-                ws_client.dropConnection(True)
+                sub_ws_clients[math.floor(self.count/2)].dropConnection(True)
+
+        def pub_callback():
+            publisher.publish(String(data=content))
+
+        pub_timer = node.create_timer(duration * 2, pub_callback)
+        timer = node.create_timer(duration, callback)
+        return future
 
 
     @websocket_test
     async def test_smoke(self, node: Node, make_client):
-        WARMUP_DELAY = 1.0  # seconds
         # Make a single publisher that pushes lots of data
-        # pub_ws_client = await make_client()
+        WARMUP_DELAY = 1.0  # seconds
         NUM_MSGS = 10
         MSG_SIZE = 10000
         A_TOPIC = "/a_topic"
@@ -59,12 +71,9 @@ class TestWebsocketSmoke(unittest.TestCase):
 
         await sleep(node, WARMUP_DELAY)
 
-        # https://python.plainenglish.io/how-to-avoid-issues-when-waiting-on-multiple-events-in-python-asyncio-48e22d148de7
-        pub_task = asyncio.create_task(pub_many_times(pub_a, A_STRING), name="publishing")
-        sub_task = asyncio.create_task(sub_many_times(A_TOPIC, make_client), name="subscribing")
-
-        done, pending = await asyncio.wait([pub_task, sub_task], return_when=asyncio.FIRST_COMPLETED)
-        for task in pending:
-            task.cancel()
+        sub_ws_clients = []
+        for _ in range(7):
+            sub_ws_clients.append(await make_client())
+        await self.sub_many(node, 0.0001, A_TOPIC, sub_ws_clients, pub_a, A_STRING)
 
         node.destroy_publisher(pub_a)
