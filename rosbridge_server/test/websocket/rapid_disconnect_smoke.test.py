4db025539a6b7f9402f036137a835226e05e3239
William Wedler <wwedler@riseup.net>
2022-05-18 08:45:29 -0400

    assertion that never gets reached

--- rosbridge_server/test/websocket/rapid_disconnect_smoke.test.py
+++ rosbridge_server/test/websocket/rapid_disconnect_smoke.test.py
@@ -21,19 +21,19 @@ log.startLogging(sys.stderr)
 generate_test_description = common.generate_test_description
 
 class TestWebsocketSmoke(unittest.TestCase):
-    def sub_many(self, node: Node, duration: float, topic: str, sub_ws_clients, publisher, content) -> Awaitable[None]:
+    def pub_sub_many(self, node: Node, duration: float, topic: str, sub_ws_clients, publisher, content) -> Awaitable[bool]:
         """
-        Async-compatible delay function based on a ROS timer.
+        Async-compatible function to publish and subscribe many times using a ROS timer.
         """
         future = rclpy.task.Future()
         self.count = 0
 
         node.get_logger().info('sub many')
-        def callback():
+        def sub_callback():
             node.get_logger().info('{}'.format(self.count))
             self.count = self.count + 1
             if self.count >= len(sub_ws_clients) * 2:
-                future.set_result(None)
+                future.set_result(True)
                 timer.cancel()
                 pub_timer.cancel()
                 node.destroy_timer(timer)
@@ -48,14 +48,13 @@ class TestWebsocketSmoke(unittest.TestCase):
                     }
                 )
             else:
-                #client.sendClose()
                 sub_ws_clients[math.floor(self.count/2)].dropConnection(True)
 
         def pub_callback():
             publisher.publish(String(data=content))
 
         pub_timer = node.create_timer(duration * 2, pub_callback)
-        timer = node.create_timer(duration, callback)
+        timer = node.create_timer(duration, sub_callback)
         return future
 
 
@@ -69,11 +68,10 @@ class TestWebsocketSmoke(unittest.TestCase):
         A_STRING = "A" * MSG_SIZE
         pub_a = node.create_publisher(String, A_TOPIC, NUM_MSGS)
 
-        await sleep(node, WARMUP_DELAY)
-
         sub_ws_clients = []
         for _ in range(7):
             sub_ws_clients.append(await make_client())
-        await self.sub_many(node, 0.0001, A_TOPIC, sub_ws_clients, pub_a, A_STRING)
+        result = await self.pub_sub_many(node, 0.0001, A_TOPIC, sub_ws_clients, pub_a, A_STRING)
+        self.assertTrue(result)
 
         node.destroy_publisher(pub_a)
