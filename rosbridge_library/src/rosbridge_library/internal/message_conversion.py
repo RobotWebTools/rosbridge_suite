d0429c33e32a526e8efb577fc1b9f6ec376ebc54
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-08-16 11:43:12 -0700

    comments

--- rosbridge_library/src/rosbridge_library/internal/message_conversion.py
+++ rosbridge_library/src/rosbridge_library/internal/message_conversion.py
@@ -360,10 +360,10 @@ def _to_list_inst(msg, rostype, roottype, inst, stack):
     # Special mappings for numeric types https://design.ros2.org/articles/idl_interface_definition.html
     if isinstance(inst, array.array):
         del inst[:]
-        inst.extend(msg)
+        inst.extend(msg)  # accepts both ints and floats which may come from json
         return inst
     if isinstance(inst, np.ndarray):
-        inst[:] = msg
+        inst[:] = msg  # accepts both ints and floats which may come from json
         return inst
 
     # Remove the list indicators from the rostype
