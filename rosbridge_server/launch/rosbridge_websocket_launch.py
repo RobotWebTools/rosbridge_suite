ecce4151b3905f62239c0a8db643c3a79eebeb59
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-09-02 17:37:49 +0100

    Update python launch file for rosbridge_server
    - Fix parameter types.
    - Add parameters for ssl launch.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- rosbridge_server/launch/rosbridge_websocket_launch.py
+++ rosbridge_server/launch/rosbridge_websocket_launch.py
@@ -7,45 +7,61 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # defaults
-    port="9090"
+    port=9090
     address=""
-    ssl="false"
+    ssl=False
     certfile=""
     keyfile=""
-    retry_startup_delay="5.0"
-    fragment_timeout="600"
-    delay_between_messages="0"
-    max_message_size="10000000"
-    unregister_timeout="10.0"
-    use_compression="false"
+    retry_startup_delay=5.0
+    fragment_timeout=600
+    delay_between_messages=0
+    max_message_size=10000000
+    unregister_timeout=10.0
+    use_compression=False
     topics_glob=""
     services_glob=""
     params_glob=""
-    bson_only_mode=""
+    bson_only_mode=False
 
-    # Package Directories
-    pkg_rosbridge_server = get_package_share_directory("rosbridge_server")
+    # parameters for the rosbridge_websocket node
+    if ssl:
+        rosbridge_websocket_params = [{
+            "certfile": certfile,
+            "keyfile": keyfile,
+            "port": port,
+            "address": address,
+            "retry_startup_delay": retry_startup_delay,
+            "fragment_timeout": fragment_timeout,
+            "delay_between_messages": delay_between_messages,
+            "max_message_size": max_message_size,
+            "unregister_timeout": unregister_timeout,
+            "use_compression": use_compression,
+            "topics_glob": topics_glob,
+            "services_glob": services_glob,
+            "params_glob": params_glob,
+        }]
+    else:
+        rosbridge_websocket_params = [{
+            "port": port,
+            "address": address,
+            "retry_startup_delay": retry_startup_delay,
+            "fragment_timeout": fragment_timeout,
+            "delay_between_messages": delay_between_messages,
+            "max_message_size": max_message_size,
+            "unregister_timeout": unregister_timeout,
+            "use_compression": use_compression,
+            "topics_glob": topics_glob,
+            "services_glob": services_glob,
+            "params_glob": params_glob,
+            "bson_only_mode": bson_only_mode,
+        }]
 
-    # TODO add ssl version and condition...
     rosbridge_websocket = Node(
         package="rosbridge_server",
         executable="rosbridge_websocket",
         name="rosbridge_websocket",
         arguments=[],
-        parameters=[{
-          "port": port,
-          "address": address,
-          "retry_startup_delay": retry_startup_delay,
-          "fragment_timeout": fragment_timeout,
-          "delay_between_messages": delay_between_messages,
-          "max_message_size": max_message_size,
-          "unregister_timeout": unregister_timeout,
-          "use_compression": use_compression,
-          "topics_glob": topics_glob,
-          "services_glob": services_glob,
-          "params_glob": params_glob,
-          "bson_only_mode": bson_only_mode,
-        }],
+        parameters=rosbridge_websocket_params,
         remappings=[],
         output="screen",
     )
@@ -64,7 +80,7 @@ def generate_launch_description():
         output="screen",
     )
 
-    # Nodes and Launches
+    # nodes and launches
     return LaunchDescription(
         [
           rosbridge_websocket,
