613fe1fdca2c5d1af6339e8af6cfbc01407f5312
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-09-03 12:24:35 +0100

    Apply formatting rules to launch file
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- rosbridge_server/launch/rosbridge_websocket_launch.py
+++ rosbridge_server/launch/rosbridge_websocket_launch.py
@@ -5,56 +5,61 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     # defaults
-    port=9090
-    address=""
-    ssl=False
-    certfile=""
-    keyfile=""
-    retry_startup_delay=5.0
-    fragment_timeout=600
-    delay_between_messages=0
-    max_message_size=10000000
-    unregister_timeout=10.0
-    use_compression=False
-    topics_glob=""
-    services_glob=""
-    params_glob=""
-    bson_only_mode=False
+    port = 9090
+    address = ""
+    ssl = False
+    certfile = ""
+    keyfile = ""
+    retry_startup_delay = 5.0
+    fragment_timeout = 600
+    delay_between_messages = 0
+    max_message_size = 10000000
+    unregister_timeout = 10.0
+    use_compression = False
+    topics_glob = ""
+    services_glob = ""
+    params_glob = ""
+    bson_only_mode = False
 
     # parameters for the rosbridge_websocket node
     if ssl:
-        rosbridge_websocket_params = [{
-            "certfile": certfile,
-            "keyfile": keyfile,
-            "port": port,
-            "address": address,
-            "retry_startup_delay": retry_startup_delay,
-            "fragment_timeout": fragment_timeout,
-            "delay_between_messages": delay_between_messages,
-            "max_message_size": max_message_size,
-            "unregister_timeout": unregister_timeout,
-            "use_compression": use_compression,
-            "topics_glob": topics_glob,
-            "services_glob": services_glob,
-            "params_glob": params_glob,
-        }]
+        rosbridge_websocket_params = [
+            {
+                "certfile": certfile,
+                "keyfile": keyfile,
+                "port": port,
+                "address": address,
+                "retry_startup_delay": retry_startup_delay,
+                "fragment_timeout": fragment_timeout,
+                "delay_between_messages": delay_between_messages,
+                "max_message_size": max_message_size,
+                "unregister_timeout": unregister_timeout,
+                "use_compression": use_compression,
+                "topics_glob": topics_glob,
+                "services_glob": services_glob,
+                "params_glob": params_glob,
+            }
+        ]
     else:
-        rosbridge_websocket_params = [{
-            "port": port,
-            "address": address,
-            "retry_startup_delay": retry_startup_delay,
-            "fragment_timeout": fragment_timeout,
-            "delay_between_messages": delay_between_messages,
-            "max_message_size": max_message_size,
-            "unregister_timeout": unregister_timeout,
-            "use_compression": use_compression,
-            "topics_glob": topics_glob,
-            "services_glob": services_glob,
-            "params_glob": params_glob,
-            "bson_only_mode": bson_only_mode,
-        }]
+        rosbridge_websocket_params = [
+            {
+                "port": port,
+                "address": address,
+                "retry_startup_delay": retry_startup_delay,
+                "fragment_timeout": fragment_timeout,
+                "delay_between_messages": delay_between_messages,
+                "max_message_size": max_message_size,
+                "unregister_timeout": unregister_timeout,
+                "use_compression": use_compression,
+                "topics_glob": topics_glob,
+                "services_glob": services_glob,
+                "params_glob": params_glob,
+                "bson_only_mode": bson_only_mode,
+            }
+        ]
 
     rosbridge_websocket = Node(
         package="rosbridge_server",
@@ -71,11 +76,13 @@ def generate_launch_description():
         executable="rosapi_node",
         name="rosapi",
         arguments=[],
-        parameters=[{
-            "topics_glob": topics_glob,
-            "services_glob": services_glob,
-            "params_glob": params_glob,
-        }],
+        parameters=[
+            {
+                "topics_glob": topics_glob,
+                "services_glob": services_glob,
+                "params_glob": params_glob,
+            }
+        ],
         remappings=[],
         output="screen",
     )
@@ -83,7 +90,7 @@ def generate_launch_description():
     # nodes and launches
     return LaunchDescription(
         [
-          rosbridge_websocket,
-          rosapi,
+            rosbridge_websocket,
+            rosapi,
         ]
     )
