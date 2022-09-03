63abb1bf2dd315876484f7db76bcf3382cc199c2
sathak93 <sathak0730@gmail.com>
2022-09-03 21:01:26 +0530

    added loader for action class and instances

--- rosbridge_library/src/rosbridge_library/internal/ros_loader.py
+++ rosbridge_library/src/rosbridge_library/internal/ros_loader.py
@@ -44,8 +44,10 @@ Methods typically return the requested class or instance, or None if not found
 # Variable containing the loaded classes
 _loaded_msgs = {}
 _loaded_srvs = {}
+_loaded_actions = {}
 _msgs_lock = Lock()
 _srvs_lock = Lock()
+_actions_lock = Lock()
 
 
 class InvalidTypeStringException(Exception):
@@ -84,6 +86,11 @@ def get_service_class(typestring):
     Returns the loaded class, or None on failure"""
     return _get_srv_class(typestring)
 
+def get_action_class(typestring):
+    """Loads the action type specified.
+
+    Returns the loaded class, or throws exceptions on failure"""
+    return _get_action_class(typestring)
 
 def get_message_instance(typestring):
     """If not loaded, loads the specified type.
@@ -100,6 +107,27 @@ def get_service_request_instance(typestring):
 def get_service_response_instance(typestring):
     cls = get_service_class(typestring)
     return cls.Response()
+
+def get_action_goal_instance(typestring):
+    cls = get_action_class(typestring)
+    return cls.Goal()
+
+
+def get_action_feedback_instance(typestring):
+    cls = get_action_class(typestring)
+    return cls.Feedback()
+
+def get_action_result_instance(typestring):
+    cls = get_action_class(typestring)
+    return cls.Result()
+
+def get_action_cancel_instance():
+    cls = get_message_class('actionlib_msgs/GoalID')
+    return cls()
+
+def get_action_status_instance():
+    cls = get_message_class('actionlib_msgs/GoalStatusArray')
+    return cls()
 
 
 def _get_msg_class(typestring):
@@ -121,7 +149,7 @@ def _get_msg_class(typestring):
 
         return _get_class(typestring, subname, _loaded_msgs, _msgs_lock)
     except (InvalidModuleException, InvalidClassException):
-        return _get_class(typestring, "msg", _loaded_msgs, _msgs_lock)
+        return _get_class(typestring, subname, _loaded_msgs, _msgs_lock)
 
 
 def _get_srv_class(typestring):
@@ -143,7 +171,28 @@ def _get_srv_class(typestring):
 
         return _get_class(typestring, subname, _loaded_srvs, _srvs_lock)
     except (InvalidModuleException, InvalidClassException):
-        return _get_class(typestring, "srv", _loaded_srvs, _srvs_lock)
+        return _get_class(typestring, subname, _loaded_srvs, _srvs_lock)
+
+def _get_action_class(typestring):
+    """If not loaded, loads the specified action class then returns an instance
+    of it
+
+    Throws various exceptions if loading the action class fails"""
+    global _loaded_actions, _actions_lock
+    try:
+        # The type string starts with the package and ends with the
+        # class and contains module subnames in between. For
+        # compatibility with ROS1 style types, we fall back to use a
+        # standard "srv" subname.
+        splits = [x for x in typestring.split("/") if x]
+        if len(splits) > 2:
+            subname = ".".join(splits[1:-1])
+        else:
+            subname = "action"
+
+        return _get_class(typestring, subname, _loaded_actions, _actions_lock)
+    except (InvalidModuleException, InvalidClassException):
+        return _get_class(typestring, subname, _loaded_actions, _actions_lock)
 
 
 def _get_class(typestring, subname, cache, lock):
