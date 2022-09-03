fdb38b4c532f9c4aafa18e4d96540c841411f0e0
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-09-03 12:24:12 +0100

    Apply formatting rules to setup.py
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- rosbridge_library/setup.py
+++ rosbridge_library/setup.py
@@ -2,28 +2,26 @@ import os
 from glob import glob
 from setuptools import setup
 
-package_name = 'rosbridge_library'
+package_name = "rosbridge_library"
 
 setup(
     name=package_name,
-    version='1.3.0',
+    version="1.3.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    author='Jonathan Mace',
-    author_email='jonathan.c.mace@gmail.com',
-    maintainer='Jihoon Lee',
-    maintainer_email='jihoonlee.in@gmail.com',
-    description='The core rosbridge package, responsible for interpreting JSON andperforming the appropriate ROS action, like subscribe, publish, call service, and interact with params.',
-    license='BSD',
-    tests_require=['pytest'],
+    author="Jonathan Mace",
+    author_email="jonathan.c.mace@gmail.com",
+    maintainer="Jihoon Lee",
+    maintainer_email="jihoonlee.in@gmail.com",
+    description="The core rosbridge package, responsible for interpreting JSON andperforming the appropriate ROS action, like subscribe, publish, call service, and interact with params.",
+    license="BSD",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
--- rosbridge_server/setup.py
+++ rosbridge_server/setup.py
@@ -2,33 +2,35 @@ import os
 from glob import glob
 from setuptools import setup
 
-package_name = 'rosbridge_server'
+package_name = "rosbridge_server"
 
 setup(
     name=package_name,
-    version='1.3.0',
+    version="1.3.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, [
-            'launch/rosbridge_websocket_launch.py',
-            'launch/rosbridge_websocket_launch.xml',
-        ]),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            "share/" + package_name,
+            [
+                "launch/rosbridge_websocket_launch.py",
+                "launch/rosbridge_websocket_launch.xml",
+            ],
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    author='Jonathan Mace',
-    author_email='jonathan.c.mace@gmail.com',
-    maintainer='Jihoon Lee',
-    maintainer_email='jihoonlee.in@gmail.com',
-    description='A WebSocket interface to rosbridge.',
-    license='BSD',
-    tests_require=['pytest'],
+    author="Jonathan Mace",
+    author_email="jonathan.c.mace@gmail.com",
+    maintainer="Jihoon Lee",
+    maintainer_email="jihoonlee.in@gmail.com",
+    description="A WebSocket interface to rosbridge.",
+    license="BSD",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'rosbridge_websocket = rosbridge_server.rosbridge_websocket:main',
+        "console_scripts": [
+            "rosbridge_websocket = rosbridge_server.rosbridge_websocket:main",
         ],
     },
 )
