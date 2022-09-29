253b1b1dc7c04e36caa3c6553293ee99e8626a8e
Hans-Joachim Krauch <achim.krauch@gmail.com>
2022-09-29 15:59:42 -0300

    Add caching for subscriptions with cbor compression.

--- rosbridge_library/src/rosbridge_library/internal/outgoing_message.py
+++ rosbridge_library/src/rosbridge_library/internal/outgoing_message.py
@@ -1,5 +1,10 @@
 from rosbridge_library.internal.message_conversion import extract_values as extract_json_values
 from rosbridge_library.internal.cbor_conversion import extract_cbor_values
+from rospy import get_rostime
+try:
+    from cbor import dumps as encode_cbor
+except ImportError:
+    from rosbridge_library.util.cbor import dumps as encode_cbor
 
 
 class OutgoingMessage:
@@ -8,6 +13,8 @@ class OutgoingMessage:
         self._message = message
         self._json_values = None
         self._cbor_values = None
+        self._cbor_msg = None
+        self._cbor_raw_msg = None
 
     @property
     def message(self):
@@ -22,3 +29,22 @@ class OutgoingMessage:
         if self._cbor_values is None:
             self._cbor_values = extract_cbor_values(self._message)
         return self._cbor_values
+
+    def get_cbor(self, outgoing_msg):
+        if self._cbor_msg is None:
+            outgoing_msg[u"msg"] = self.get_cbor_values()
+            self._cbor_msg = encode_cbor(outgoing_msg)
+
+        return self._cbor_msg
+
+    def get_cbor_raw(self, outgoing_msg):
+        if self._cbor_raw_msg is None:
+            now = get_rostime()
+            outgoing_msg[u"msg"] = {
+                u"secs": now.secs,
+                u"nsecs": now.nsecs,
+                u"bytes": self._message._buff
+            }
+            self._cbor_raw_msg = encode_cbor(outgoing_msg)
+
+        return self._cbor_raw_msg
