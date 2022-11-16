18124f0060cd3145215fd7695b51fcb7dddf3fe6
sathak93 <sathak0730@gmail.com>
2022-11-16 10:16:53 +0530

    create Client per name

--- rosbridge_library/src/rosbridge_library/capabilities/action_client.py
+++ rosbridge_library/src/rosbridge_library/capabilities/action_client.py
@@ -18,9 +18,9 @@ class ActionClientRequests(Capability):
         (False, "compression", str),
     ]
 
-    destroy_client_msg_fields = [(True, "action_type", str)]
+    destroy_client_msg_fields = [(True, "action_name", str)]
 
-    cancel_goal_msg_fields = [(True, "action_type", str)]
+    cancel_goal_msg_fields = [(True, "action_name", str)]
 
     actions_glob = None
 
@@ -45,11 +45,11 @@ class ActionClientRequests(Capability):
 
         if ActionClientRequests.actions_glob is not None and ActionClientRequests.actions_glob:
             self.protocol.log(
-                "debug", "Action security glob enabled, checking action: " + action_type
+                "debug", "Action security glob enabled, checking action clients: " + action_name
             )
             match = False
             for glob in ActionClientRequests.actions_glob:
-                if fnmatch.fnmatch(action_type, glob):
+                if fnmatch.fnmatch(action_name, glob):
                     self.protocol.log(
                         "debug",
                         "Found match with glob " + glob + ", creating Action client",
@@ -59,33 +59,33 @@ class ActionClientRequests(Capability):
             if not match:
                 self.protocol.log(
                     "warn",
-                    "No match found for action, cancelling creation of action client type: "
-                    + action_type,
+                    "No match found for action, cancelling creation of action client: "
+                    + action_name,
                 )
                 return
         else:
             self.protocol.log("debug", "No action security glob, not checking Action client.")
 
         client_id = msg.get("id", None)
-        s_cb = partial(self._success, client_id, action_type)
-        e_cb = partial(self._failure, client_id, action_type)
+        s_cb = partial(self._success, client_id, action_name)
+        e_cb = partial(self._failure, client_id, action_name)
         if feedback:
-            f_cb = partial(self._feedback, client_id, action_type)
+            f_cb = partial(self._feedback, client_id, action_name)
         else:
             f_cb = None
 
-        if action_type not in self._actionclients:
-            self._actionclients[action_type] = ActionClientHandle(
+        if action_name not in self._actionclients:
+            self._actionclients[action_name] = ActionClientHandle(
                 action_name, action_type, self.protocol.node_handle
             )
 
-        GoalHandle(self._actionclients[action_type], goal_msg, s_cb, e_cb, f_cb).start()
+        GoalHandle(self._actionclients[action_name], goal_msg, s_cb, e_cb, f_cb).start()
 
-    def _success(self, cid, action_type, message):
+    def _success(self, cid, action_name, message):
         outgoing_message = {
             "op": "action_response",
             "response_type": "result",
-            "type": action_type,
+            "name": action_name,
             "values": message,
         }
         if cid is not None:
@@ -93,11 +93,11 @@ class ActionClientRequests(Capability):
         # TODO: fragmentation, compression
         self.protocol.send(outgoing_message)
 
-    def _failure(self, cid, action_type, exc):
+    def _failure(self, cid, action_name, exc):
         outgoing_message = {
             "op": "action_response",
             "response_type": "error",
-            "type": action_type,
+            "name": action_name,
             "values": str(exc),
         }
         if cid is not None:
@@ -105,11 +105,11 @@ class ActionClientRequests(Capability):
         # TODO: fragmentation, compression
         self.protocol.send(outgoing_message)
 
-    def _feedback(self, cid, action_type, message):
+    def _feedback(self, cid, action_name, message):
         outgoing_message = {
             "op": "action_response",
             "response_type": "feedback",
-            "type": action_type,
+            "name": action_name,
             "values": extract_values(message),
         }
         if cid is not None:
@@ -119,16 +119,16 @@ class ActionClientRequests(Capability):
 
     def destroy_client(self, msg):
         self.basic_type_check(msg, self.destroy_client_msg_fields)
-        action_type = msg.get("action_type")
+        action_name = msg.get("action_name")
 
         if ActionClientRequests.actions_glob is not None and ActionClientRequests.actions_glob:
             self.protocol.log(
                 "debug",
-                "Action security glob enabled, checking action client of type:: " + action_type,
+                "Action security glob enabled, checking action clients: " + action_name,
             )
             match = False
             for glob in ActionClientRequests.actions_glob:
-                if fnmatch.fnmatch(action_type, glob):
+                if fnmatch.fnmatch(action_name, glob):
                     self.protocol.log(
                         "debug",
                         "Found match with glob " + glob + ", killing client",
@@ -138,46 +138,46 @@ class ActionClientRequests(Capability):
             if not match:
                 self.protocol.log(
                     "warn",
-                    "No match found for action client, cancelling destruction of action client of type: "
-                    + action_type,
+                    "No match found for action client, cancelling destruction of action client: "
+                    + action_name,
                 )
                 return
         else:
             self.protocol.log("debug", "No action security glob, not checking Action client.")
 
-        if action_type not in self._actionclients:
-            self.protocol.log("info", "action client of type %s not available" % action_type)
+        if action_name not in self._actionclients:
+            self.protocol.log("info", "action client %s not available" % action_name)
             return
-        self._actionclients[action_type].unregister()
-        del self._actionclients[action_type]
+        self._actionclients[action_name].unregister()
+        del self._actionclients[action_name]
 
         if len(self._actionclients) == 0:
             self._actionclients.clear()
 
-        self.protocol.log("info", "Destroyed Action Client of type %s" % action_type)
+        self.protocol.log("info", "Destroyed Action Client %s" % action_name)
 
     def cancel_goal(self, msg):
         self.basic_type_check(msg, self.cancel_goal_msg_fields)
-        action_type = msg.get("action_type")
+        action_name = msg.get("action_name")
         cid = msg.get("id", None)
 
-        if action_type not in self._actionclients:
-            self.protocol.log("info", "action client of type %s not available" % action_type)
+        if action_name not in self._actionclients:
+            self.protocol.log("info", "action client %s not available" % action_name)
             return
 
-        result = self._actionclients[action_type].cancel_goal_call()
+        result = self._actionclients[action_name].cancel_goal_call()
 
         outgoing_message = {
             "op": "action_response",
             "response_type": "cancel",
-            "type": action_type,
+            "name": action_name,
             "values": result,
         }
         if cid is not None:
             outgoing_message["id"] = cid
         self.protocol.send(outgoing_message)
 
-        self.protocol.log("info", "cancelled goal %s" % action_type)
+        self.protocol.log("info", "cancelled goals of %s" % action_name)
 
     def finish(self):
         for clients in self._actionclients.values():
