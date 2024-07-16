88534ab5bddf1cb97ad16b07da3f42cb3843607c
Eric Gallimore <egallimore@whoi.edu>
2024-07-16 17:50:40 -0400

    Fix setup.py data_files.

--- rosbridge_server/setup.py
+++ rosbridge_server/setup.py
@@ -8,7 +8,17 @@ setup(
     name=package_name,
     version="1.3.2",
     packages=find_packages(exclude=["test"]),
-    data_files=[(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))],
+    data_files=[
+        # Install marker file in the package index
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        # Include our package.xml file
+        (os.path.join("share", package_name), ["package.xml"]),
+        # Include all launch files.
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
+    ],
     install_requires=["setuptools"],
     zip_safe=True,
     author="Jonathan Mace",
