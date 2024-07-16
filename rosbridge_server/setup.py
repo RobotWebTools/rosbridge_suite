28eb1c0a2acea66f383de3298d51522059c7fa52
Eric Gallimore <egallimore@whoi.edu>
2024-07-16 14:37:32 -0400

    Change rosbridge_server to use ament_python rather than ament_cmake.

---
+++ rosbridge_server/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'rosbridge_server'
+
+setup(
+ name=package_name,
+ version='1.3.2',
+ packages=find_packages(exclude=['test']),
+ data_files=[
+   ],
+ install_requires=['setuptools'],
+ zip_safe=True,
+ author='Jonathan Mace',
+ author_email='jonathan.c.mace@gmail.com',
+ maintainer='Jihoon Lee, Foxglove',
+ maintainer_email='jihoonlee.in@gmail.com, ros-tooling@foxglove.dev',
+ description='A WebSocket interface to rosbridge.',
+ license='BSD',
+ tests_require=['pytest'],
+ entry_points={
+     'console_scripts': [
+             'rosbridge_websocket = rosbridge_server.rosbridge_websocket:main'
+     ],
+   },
+)
