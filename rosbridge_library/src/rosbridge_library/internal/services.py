ad7332b784c51e67f0da24ebe70d9018d696deb8
Błażej Sowa <bsowa123@gmail.com>
2025-01-09 00:52:47 +0100

    Run isort

--- rosbridge_library/src/rosbridge_library/internal/services.py
+++ rosbridge_library/src/rosbridge_library/internal/services.py
@@ -30,7 +30,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from threading import Thread, Condition
+from threading import Condition, Thread
 from typing import Any, Callable, Optional
 
 from rclpy.callback_groups import ReentrantCallbackGroup
