aa7a0c813adf959e38c82d79ecaa996a71b80a89
Błażej Sowa <bsowa123@gmail.com>
2025-08-06 23:22:58 +0000

    Enable flake8-future-annotations and fix lint errors

--- rosbridge_library/src/rosbridge_library/internal/services.py
+++ rosbridge_library/src/rosbridge_library/internal/services.py
@@ -29,6 +29,7 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+from __future__ import annotations
 
 from threading import Event, Thread
 from typing import TYPE_CHECKING, Any, Callable, Optional
