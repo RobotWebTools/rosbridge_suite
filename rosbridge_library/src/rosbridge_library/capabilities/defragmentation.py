7b1f25eca924ac5c45417e9bd25f6bad2055b2f2
Błażej Sowa <bsowa123@gmail.com>
2025-08-15 15:06:25 +0200

    Add type annotations and simplify logging in defragmentation

--- rosbridge_library/src/rosbridge_library/capabilities/defragmentation.py
+++ rosbridge_library/src/rosbridge_library/capabilities/defragmentation.py
@@ -1,7 +1,11 @@
-import threading
 import time
+from typing import TYPE_CHECKING, Any
 
 from rosbridge_library.capability import Capability
+from rosbridge_library.protocol import Protocol
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
 
 
 class ReceivedFragments:
@@ -10,7 +14,7 @@ class ReceivedFragments:
     class __impl:
         """Implementation of the singleton interface."""
 
-        def spam(self):
+        def spam(self) -> int:
             """Test method, return singleton id."""
             return id(self)
 
@@ -30,33 +34,21 @@ class ReceivedFragments:
     # ...
     lists: dict[str, dict]
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Create singleton instance."""
         if ReceivedFragments.__instance is None:
             ReceivedFragments.__instance = ReceivedFragments.__impl()
-            self.lists = {}
+            self.lists: dict[str, Any] = {}
 
         self.__dict__["_ReceivedFragments__instance"] = ReceivedFragments.__instance
 
-    def __getattr__(self, attr):
-        """Delegate access to implementation."""
-        return getattr(self.__instance, attr)
 
-    def __setattr__(self, attr, value):
-        """Delegate access to implementation."""
-        return setattr(self.__instance, attr, value)
-
-
-class Defragment(Capability, threading.Thread):
+class Defragment(Capability):
     fragment_timeout = 600
     opcode = "fragment"
 
-    protocol = None
-
-    def __init__(self, protocol):
+    def __init__(self, protocol: Protocol) -> None:
         Capability.__init__(self, protocol)
-
-        self.protocol = protocol
 
         # populate parameters
         if self.protocol.parameters is not None:
@@ -65,7 +57,6 @@ class Defragment(Capability, threading.Thread):
         protocol.register_operation(self.opcode, self.defragment)
 
         self.received_fragments = ReceivedFragments().lists
-        threading.Thread.__init__(self)
 
     # defragment() does:
     #   1) take any incoming message with op-code "fragment"
@@ -84,52 +75,53 @@ class Defragment(Capability, threading.Thread):
     #       (protocol.incoming is checking message fields by itself, so no need to do this before
     #       passing the reconstructed message to protocol)
     #   4.c) remove the fragment list to free up memory
-    def defragment(self, message):
+    def defragment(self, message: dict[str, Any]) -> None:
         now = time.monotonic()
 
-        if self.received_fragments is not None:
-            for frag_id in self.received_fragments:
-                time_diff = now - self.received_fragments[frag_id]["timestamp_last_append"]
-                if (
-                    time_diff > self.fragment_timeout
-                    and not self.received_fragments[frag_id]["is_reconstructing"]
-                ):
-                    log_msg = ["fragment list ", str(frag_id), " timed out.."]
+        msg_id: str | None = message.get("id")
+        if msg_id is None:
+            self.protocol.log("error", "received invalid fragment! No message ID found.")
+            return
 
-                    if message["id"] != frag_id:
-                        log_msg.append(" -> removing it..")
-                        del self.received_fragments[frag_id]
-                    else:
-                        log_msg.extend([" -> but we're just about to add fragment #"])
-                        log_msg.extend([str(message.get("num")), " of "])
-                        log_msg.extend([str(self.received_fragments[message.get("id")]["total"])])
-                        log_msg.extend([" ..keeping the list"])
-                    self.protocol.log("warning", "".join(log_msg))
+        for frag_id in self.received_fragments:
+            time_diff = now - self.received_fragments[frag_id]["timestamp_last_append"]
+            if (
+                time_diff > self.fragment_timeout
+                and not self.received_fragments[frag_id]["is_reconstructing"]
+            ):
+                log_msg = [f"fragment list {frag_id} timed out.."]
 
-        msg_opcode = message.get("op")
-        msg_id = message.get("id")
-        msg_num = message.get("num")
-        msg_total = message.get("total")
-        msg_data = message.get("data")
+                if msg_id != frag_id:
+                    log_msg.append(" -> removing it..")
+                    del self.received_fragments[frag_id]
+                else:
+                    log_msg.append(
+                        f" -> but we're just about to add fragment #{message.get('num')}"
+                        f" of {self.received_fragments[msg_id]['total']} ..keeping the list"
+                    )
+                self.protocol.log("warning", "".join(log_msg))
+
+        msg_opcode: str | None = message.get("op")
+        msg_num: int | None = message.get("num")
+        msg_total: int | None = message.get("total")
+        msg_data: Sequence | None = message.get("data")
 
         # Abort if any message field is missing
-        if None in (msg_opcode, msg_id, msg_num, msg_total, msg_data):
+        if msg_opcode is None or msg_num is None or msg_total is None or msg_data is None:
             self.protocol.log("error", "received invalid fragment!")
             return
 
-        log_msg = "fragment for messageID: " + str(msg_id) + " received."
-        self.protocol.log("debug", log_msg)
+        self.protocol.log("debug", "fragment for messageID: " + str(msg_id) + " received.")
 
         # Create fragment container if none exists yet
         if msg_id not in self.received_fragments:
             self.received_fragments[msg_id] = {
                 "is_reconstructing": False,
-                "total": message["total"],
+                "total": msg_total,
                 "timestamp_last_append": now,
                 "fragment_list": {},
             }
-            log_msg = "opened new fragment list for messageID " + str(msg_id)
-            self.protocol.log("debug", log_msg)
+            self.protocol.log("debug", "opened new fragment list for messageID " + str(msg_id))
 
         # print "received fragments:", len(self.received_fragments[msg_id]["fragment_list"].keys())
 
@@ -141,19 +133,13 @@ class Defragment(Capability, threading.Thread):
         ):
             self.received_fragments[msg_id]["fragment_list"][msg_num] = msg_data
             self.received_fragments[msg_id]["timestamp_last_append"] = now
-            log_msg = ["appended fragment #" + str(msg_num)]
-            log_msg.extend(
-                [
-                    " (total: ",
-                    str(msg_total),
-                    ") to fragment list for messageID ",
-                    str(msg_id),
-                ]
+
+            self.protocol.log(
+                "debug",
+                f"appended fragment #{msg_num} (total: {msg_total}) to fragment list for messageID {msg_id}",
             )
-            self.protocol.log("debug", "".join(log_msg))
         else:
-            log_msg = "error while trying to append fragment " + str(msg_num)
-            self.protocol.log("error", log_msg)
+            self.protocol.log("error", "error while trying to append fragment " + str(msg_num))
             return
 
         received_all_fragments = False
@@ -162,50 +148,40 @@ class Defragment(Capability, threading.Thread):
 
         # Make sure total number of fragments received
         if existing_fragments == announced_total:
-            log_msg = ["enough/all fragments for messageID " + str(msg_id) + " received"]
-            log_msg.extend([" [", str(existing_fragments), "]"])
-            log_msg = "".join(log_msg)
-            self.protocol.log("debug", log_msg)
+            self.protocol.log(
+                "debug",
+                f"enough/all fragments for messageID {msg_id} received [ {existing_fragments} ]",
+            )
             # Check each fragment matches up
             received_all_fragments = True
             for i in range(announced_total):
                 if i not in self.received_fragments[msg_id]["fragment_list"]:
                     received_all_fragments = False
-                    log_msg = "fragment #" + str(i)
-                    log_msg += " for messageID " + str(msg_id) + " is missing! "
-                    self.protocol.log("error", log_msg)
+                    self.protocol.log("error", f"fragment #{i} for messageID {msg_id} is missing!")
 
         self.received_fragments[msg_id]["is_reconstructing"] = received_all_fragments
 
         if received_all_fragments:
-            log_msg = "reconstructing original message " + str(msg_id)
-            self.protocol.log("debug", log_msg)
+            self.protocol.log("debug", f"reconstructing original message {msg_id}")
 
             # Reconstruct the message
             reconstructed_msg = "".join(self.received_fragments[msg_id]["fragment_list"].values())
 
-            log_msg = ["reconstructed original message:\n"]
-            log_msg.append(reconstructed_msg)
-            log_msg = "".join(log_msg)
-            self.protocol.log("debug", log_msg)
+            self.protocol.log("debug", f"reconstructed original message:\n{reconstructed_msg}")
 
             duration = time.monotonic() - now
 
             # Pass the reconstructed message to rosbridge
             self.protocol.incoming(reconstructed_msg)
-            log_msg = ["reconstructed message (ID:" + str(msg_id) + ") from "]
-            log_msg.extend([str(msg_total), " fragments. "])
-            # cannot access msg.data if message is a service_response or else!
-            # log_msg += "[message length: " + str(len(str(json.loads(reconstructed_msg)["msg"]["data"]))) +"]"
-            log_msg.extend(["[duration: ", str(duration), " s]"])
-            log_msg = "".join(log_msg)
-            self.protocol.log("info", log_msg)
+
+            self.protocol.log(
+                "info",
+                f"reconstructed messsage (ID: {msg_id}) from {msg_total} fragments. [duration: {duration} s]",
+            )
 
             # Remove fragmentation container
             del self.received_fragments[msg_id]
-            log_msg = "removed fragment list for messageID " + str(msg_id)
-            self.protocol.log("debug", log_msg)
+            self.protocol.log("debug", f"removed fragment list for messageID {msg_id}")
 
-    def finish(self):
-        self.received_fragments = None
+    def finish(self) -> None:
         self.protocol.unregister_operation("fragment")
