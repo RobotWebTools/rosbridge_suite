79deb3bbefb451052412ebb620be76380d482cc9
Błażej Sowa <bsowa123@gmail.com>
2025-08-15 15:35:16 +0200

    Add type annotations to fragmentation capability

--- rosbridge_library/src/rosbridge_library/capabilities/fragmentation.py
+++ rosbridge_library/src/rosbridge_library/capabilities/fragmentation.py
@@ -30,9 +30,17 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import annotations
+
 import math
+from typing import TYPE_CHECKING, Any
 
 from rosbridge_library.capability import Capability
+
+if TYPE_CHECKING:
+    from collections.abc import Generator, Iterable, Sequence
+
+    from rosbridge_library.protocol import Protocol
 
 
 class Fragmentation(Capability):
@@ -45,11 +53,13 @@ class Fragmentation(Capability):
 
     fragmentation_seed = 0
 
-    def __init__(self, protocol):
+    def __init__(self, protocol: Protocol) -> None:
         # Call superclass constructor
         Capability.__init__(self, protocol)
 
-    def fragment(self, message, fragment_size, mid=None):
+    def fragment(
+        self, message: dict[str, Any], fragment_size: int, mid: str | None = None
+    ) -> Iterable[dict[str, Any]]:
         """
         Fragment a message into smaller parts.
 
@@ -67,11 +77,11 @@ class Fragmentation(Capability):
         :param mid: (optional) if provided, the fragment messages will be given this id.
             Otherwise an id will be auto-generated.
 
-        :return: A generator of message dict objects representing the fragments
+        :return: Am iterable of ROSBridge messages, either a single message or multiple fragments.
         """
         # All fragmented messages need an ID so they can be reconstructed
         if mid is None:
-            mid = self.fragmentation_seed
+            mid = str(self.fragmentation_seed)
             self.fragmentation_seed = self.fragmentation_seed + 1
 
         serialized = self.protocol.serialize(message, mid)
@@ -101,16 +111,20 @@ class Fragmentation(Capability):
 
         return self._fragment_generator(serialized, fragment_size, mid)
 
-    def _fragment_generator(self, msg, size, mid):
+    def _fragment_generator(
+        self, msg: Sequence, size: int, mid: str
+    ) -> Generator[dict[str, Any], None, None]:
         """Return a generator of fragment messages."""
-        total = ((len(msg) - 1) / size) + 1
+        total = ((len(msg) - 1) // size) + 1
         n = 0
         for i in range(0, len(msg), size):
             fragment = msg[i : i + size]
             yield self._create_fragment(fragment, n, total, mid)
             n = n + 1
 
-    def _create_fragment(self, fragment, num, total, mid):
+    def _create_fragment(
+        self, fragment: Sequence, num: int, total: int, mid: str
+    ) -> dict[str, Any]:
         """
         Create a fragment message.
 
