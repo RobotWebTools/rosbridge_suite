55b2d61f3dca475318788ff6f5efe112769c8e98
Błażej Sowa <bsowa123@gmail.com>
2025-08-18 12:11:47 +0000

    Add type annotation to capability tests

--- rosbridge_library/test/capabilities/test_action_capabilities.py
+++ rosbridge_library/test/capabilities/test_action_capabilities.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+from __future__ import annotations
+
 import time
 import unittest
 from json import dumps, loads
 from threading import Thread
+from typing import Any
 
 import rclpy
 from action_msgs.msg import GoalStatus
@@ -24,7 +27,7 @@ from rosbridge_library.protocol import Protocol
 
 
 class TestActionCapabilities(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         rclpy.init()
         self.executor = SingleThreadedExecutor()
         self.node = Node("test_action_capabilities")
@@ -44,35 +47,35 @@ class TestActionCapabilities(unittest.TestCase):
         self.result = ActionResult(self.proto)
         self.send_goal = SendActionGoal(self.proto)
         self.feedback = ActionFeedback(self.proto)
-        self.received_message = None
-        self.log_entries = []
+        self.received_message: dict[str, Any] | bytes | None = None
+        self.log_entries: list[tuple[str, str]] = []
 
         self.exec_thread = Thread(target=self.executor.spin)
         self.exec_thread.start()
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         self.executor.remove_node(self.node)
         self.executor.shutdown()
         rclpy.shutdown()
 
-    def local_send_cb(self, msg):
+    def local_send_cb(self, msg: dict[str, Any] | bytes) -> None:
         self.received_message = msg
 
-    def feedback_subscriber_cb(self, msg):
+    def feedback_subscriber_cb(self, msg: Fibonacci_FeedbackMessage) -> None:
         self.latest_feedback = msg
 
-    def mock_log(self, loglevel, message, _=None):
+    def mock_log(self, loglevel: str, message: str, _lid: str | None = None) -> None:
         self.log_entries.append((loglevel, message))
 
-    def test_advertise_missing_arguments(self):
+    def test_advertise_missing_arguments(self) -> None:
         advertise_msg = loads(dumps({"op": "advertise_action"}))
         self.assertRaises(MissingArgumentException, self.advertise.advertise_action, advertise_msg)
 
-    def test_advertise_invalid_arguments(self):
+    def test_advertise_invalid_arguments(self) -> None:
         advertise_msg = loads(dumps({"op": "advertise_action", "type": 42, "action": None}))
         self.assertRaises(InvalidArgumentException, self.advertise.advertise_action, advertise_msg)
 
-    def test_result_missing_arguments(self):
+    def test_result_missing_arguments(self) -> None:
         result_msg = loads(dumps({"op": "action_result"}))
         self.assertRaises(MissingArgumentException, self.result.action_result, result_msg)
 
@@ -81,11 +84,11 @@ class TestActionCapabilities(unittest.TestCase):
         result_msg = loads(dumps({"op": "action_result", "id": "dummy_action", "values": "none"}))
         self.assertRaises(MissingArgumentException, self.result.action_result, result_msg)
 
-    def test_result_invalid_arguments(self):
+    def test_result_invalid_arguments(self) -> None:
         result_msg = loads(dumps({"op": "action_result", "action": 5, "result": "error"}))
         self.assertRaises(InvalidArgumentException, self.result.action_result, result_msg)
 
-    def test_advertise_action(self):
+    def test_advertise_action(self) -> None:
         action_path = "/fibonacci_action_1"
         advertise_msg = loads(
             dumps(
@@ -98,7 +101,7 @@ class TestActionCapabilities(unittest.TestCase):
         )
         self.advertise.advertise_action(advertise_msg)
 
-    def test_execute_advertised_action(self):
+    def test_execute_advertised_action(self) -> None:
         # Advertise the action
         action_path = "/fibonacci_action_2"
         advertise_msg = loads(
@@ -207,7 +210,7 @@ class TestActionCapabilities(unittest.TestCase):
         self.assertEqual(self.received_message["values"]["sequence"], [0, 1, 1, 2, 3, 5])
         self.assertEqual(self.received_message["status"], GoalStatus.STATUS_SUCCEEDED)
 
-    def test_cancel_advertised_action(self):
+    def test_cancel_advertised_action(self) -> None:
         # Advertise the action
         action_path = "/fibonacci_action_3"
         advertise_msg = loads(
@@ -297,7 +300,7 @@ class TestActionCapabilities(unittest.TestCase):
         self.assertEqual(self.received_message["values"]["sequence"], [])
         self.assertEqual(self.received_message["status"], GoalStatus.STATUS_CANCELED)
 
-    def test_unadvertise_action(self):
+    def test_unadvertise_action(self) -> None:
         # Advertise the action
         action_path = "/fibonacci_action_4"
         advertise_msg = loads(
