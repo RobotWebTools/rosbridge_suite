838fa13b3689055820c2bac2ee19bccdfee439a0
Błażej Sowa <bsowa123@gmail.com>
2025-08-19 01:54:22 +0200

    Ignore no-redef mypy errors

--- rosbridge_library/src/rosbridge_library/util/__init__.py
+++ rosbridge_library/src/rosbridge_library/util/__init__.py
@@ -3,9 +3,9 @@ try:
     import ujson as json  # type: ignore[import-untyped]
 except ImportError:
     try:
-        import simplejson as json  # type: ignore[import-untyped]
+        import simplejson as json  # type: ignore[import-untyped, no-redef]
     except ImportError:
-        import json  # noqa: F401
+        import json  # type: ignore[no-redef] # noqa: F401
 
 import bson
 
