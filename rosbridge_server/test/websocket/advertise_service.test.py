e69fa1e07f4ac1915669aefc1b801afbf2b34d8d
Błażej Sowa <bsowa123@gmail.com>
2025-08-31 18:50:23 +0200

    Add type annotations to rosbridge_server tests

--- rosbridge_server/test/websocket/advertise_service.test.py
+++ rosbridge_server/test/websocket/advertise_service.test.py
@@ -1,14 +1,21 @@
+from __future__ import annotations
+
 import sys
 import unittest
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Callable
 
-from rclpy.node import Node
 from std_srvs.srv import SetBool
 from twisted.python import log
 
 if TYPE_CHECKING:
+    from collections.abc import Awaitable
+
+    from common import TestClientProtocol
     from rclpy.client import Client
+    from rclpy.node import Node
+    from rclpy.task import Future
+
 
 sys.path.append(str(Path(__file__).parent))  # enable importing from common.py in this directory
 
@@ -22,7 +29,9 @@ generate_test_description = common.generate_test_description
 
 class TestAdvertiseService(unittest.TestCase):
     @websocket_test
-    async def test_two_concurrent_calls(self, node: Node, make_client):
+    async def test_two_concurrent_calls(
+        self, node: Node, make_client: Callable[[], Awaitable[TestClientProtocol]]
+    ) -> None:
         ws_client = await make_client()
         ws_client.sendJson(
             {
@@ -31,20 +40,25 @@ class TestAdvertiseService(unittest.TestCase):
                 "service": "/test_service",
             }
         )
-        client: Client = node.create_client(SetBool, "/test_service")
+        client: Client = node.create_client(
+            SetBool,  # type: ignore[arg-type]
+            "/test_service",
+        )
         client.wait_for_service()
 
+        requests_future: Future[list[dict[str, Any]]]
         requests_future, ws_client.message_handler = expect_messages(
             2, "WebSocket", node.get_logger()
         )
-        assert node.executor is not None
-        requests_future.add_done_callback(lambda _: node.executor.wake())
+        executor = node.executor
+        assert executor is not None
+        requests_future.add_done_callback(lambda _: executor.wake())
 
         response1_future = client.call_async(SetBool.Request(data=True))
         response2_future = client.call_async(SetBool.Request(data=False))
 
         requests = await requests_future
-        self.assertEqual(len(requests), 2)
+        assert requests is not None and len(requests) == 2
 
         self.assertEqual(requests[0]["op"], "call_service")
         self.assertEqual(requests[0]["service"], "/test_service")
