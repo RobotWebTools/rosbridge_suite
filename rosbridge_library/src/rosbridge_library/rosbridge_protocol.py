d9340f387bf1522bdf30dac9a6292ae313e56e2c
Błażej Sowa <bsowa123@gmail.com>
2025-10-06 09:20:48 +0000

    Use super to call base class constructor

--- rosbridge_library/src/rosbridge_library/rosbridge_protocol.py
+++ rosbridge_library/src/rosbridge_library/rosbridge_protocol.py
@@ -83,6 +83,6 @@ class RosbridgeProtocol(Protocol):
     def __init__(
         self, client_id: str, node_handle: Node, parameters: dict[str, Any] | None = None
     ) -> None:
-        Protocol.__init__(self, client_id, node_handle, parameters)
+        super().__init__(client_id, node_handle, parameters)
         for capability_class in self.rosbridge_capabilities:
             self.add_capability(capability_class)
