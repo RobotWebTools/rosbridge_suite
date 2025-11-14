97a2434f5df7d8717e42c72c5f71da810510042a
Błażej Sowa <bsowa123@gmail.com>
2025-11-14 01:28:32 +0100

    feat: Replace cbor with cbor2 library, remove inline implementation (#1107)
    * feat: Replace cbor with cbor2 library, remove inline implementation
    
    * Add simple test for encoding ROS message to CBOR

--- rosbridge_library/test/internal/test_outgoing_message.py
+++ rosbridge_library/test/internal/test_outgoing_message.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import unittest
 
+from cbor2 import loads
 from rosbridge_library.internal.outgoing_message import OutgoingMessage
 from std_msgs.msg import String
 
@@ -27,6 +28,22 @@ class TestOutgoingMessage(unittest.TestCase):
 
         again = outgoing.get_cbor_values()
         self.assertTrue(result is again)
+
+    def test_cbor_encoding(self) -> None:
+        msg = String(data="foo")
+        outgoing = OutgoingMessage(msg)
+
+        outgoing_msg = {"op": "publish", "topic": "/chatter"}
+        cbor1 = outgoing.get_cbor(outgoing_msg)
+        cbor2 = outgoing.get_cbor(outgoing_msg)
+        self.assertTrue(cbor1 is cbor2)
+        self.assertIsInstance(cbor1, bytes)
+
+        decoded = loads(cbor1)
+        self.assertEqual(decoded["op"], "publish")
+        self.assertEqual(decoded["topic"], "/chatter")
+        self.assertIn("msg", decoded)
+        self.assertEqual(decoded["msg"]["data"], msg.data)
 
 
 if __name__ == "__main__":
