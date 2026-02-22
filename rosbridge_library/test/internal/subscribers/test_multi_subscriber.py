a30fbe23a70a4f0d065d50c1dbffe0ce084ab5df
Błażej Sowa <bsowa123@gmail.com>
2026-02-22 21:38:22 +0100

    fix: Failing service and subscription tests (backport #1147)
    * Add error logging when service is unadvertised during in-flight request
    
    * Fix service destruction method in AdvertisedServiceHandler
    
    * Fix unadvertise service test
    
    * Try to fix failing topic subscription assertions by adding a timeout
    
    * Remove redundant sleeps
    
    * Remove extra call to destroy service after graceful shutdown
    
    * Re-raise original exception in handle_request

--- rosbridge_library/test/internal/subscribers/test_multi_subscriber.py
+++ rosbridge_library/test/internal/subscribers/test_multi_subscriber.py
@@ -37,29 +37,43 @@ class TestMultiSubscriber(unittest.TestCase):
         self.executor.shutdown()
         rclpy.shutdown()
 
+    def assertTopicSubscribed(self, topic: str, timeout: float = 1.0) -> None:
+        start_time = time.monotonic()
+        while not is_topic_subscribed(self.node, topic):
+            time.sleep(0.05)
+            if time.monotonic() - start_time > timeout:
+                self.fail(f"Timed out waiting for topic '{topic}' to be subscribed.")
+
+    def assertTopicNotSubscribed(self, topic: str, timeout: float = 1.0) -> None:
+        start_time = time.monotonic()
+        while is_topic_subscribed(self.node, topic):
+            time.sleep(0.05)
+            if time.monotonic() - start_time > timeout:
+                self.fail(f"Timed out waiting for topic '{topic}' to be unsubscribed.")
+
     def test_register_multisubscriber(self) -> None:
         """Register a subscriber on a clean topic with a good msg type."""
         topic = "/test_register_multisubscriber"
         msg_type = "std_msgs/String"
 
-        self.assertFalse(is_topic_subscribed(self.node, topic))
+        self.assertTopicNotSubscribed(topic)
         MultiSubscriber[String](
             topic, self.client_id, lambda *_args: None, self.node, msg_type=msg_type
         )
-        self.assertTrue(is_topic_subscribed(self.node, topic))
+        self.assertTopicSubscribed(topic)
 
     def test_unregister_multisubscriber(self) -> None:
         """Register and unregister a subscriber on a clean topic with a good msg type."""
         topic = "/test_unregister_multisubscriber"
         msg_type = "std_msgs/String"
 
-        self.assertFalse(is_topic_subscribed(self.node, topic))
+        self.assertTopicNotSubscribed(topic)
         multi: MultiSubscriber[String] = MultiSubscriber(
             topic, self.client_id, lambda *_args: None, self.node, msg_type=msg_type
         )
-        self.assertTrue(is_topic_subscribed(self.node, topic))
+        self.assertTopicSubscribed(topic)
         multi.unregister()
-        self.assertFalse(is_topic_subscribed(self.node, topic))
+        self.assertTopicNotSubscribed(topic)
 
     def test_verify_type(self) -> None:
         topic = "/test_verify_type"
@@ -89,11 +103,11 @@ class TestMultiSubscriber(unittest.TestCase):
         topic = "/test_subscribe_unsubscribe"
         msg_type = "std_msgs/String"
 
-        self.assertFalse(is_topic_subscribed(self.node, topic))
+        self.assertTopicNotSubscribed(topic)
         multi: MultiSubscriber[String] = MultiSubscriber(
             topic, self.client_id, lambda *_args: None, self.node, msg_type=msg_type
         )
-        self.assertTrue(is_topic_subscribed(self.node, topic))
+        self.assertTopicSubscribed(topic)
         self.assertEqual(len(multi.new_subscriptions), 0)
 
         multi.subscribe(self.client_id, lambda _: None)
@@ -103,8 +117,7 @@ class TestMultiSubscriber(unittest.TestCase):
         self.assertEqual(len(multi.new_subscriptions), 0)
 
         multi.unregister()
-        time.sleep(0.1)
-        self.assertFalse(is_topic_subscribed(self.node, topic))
+        self.assertTopicNotSubscribed(topic)
 
     def test_subscribe_receive_json(self) -> None:
         topic = "/test_subscribe_receive_json"
