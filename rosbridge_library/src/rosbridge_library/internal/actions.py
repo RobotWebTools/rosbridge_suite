728ed0d228de384f30c4468fbe5e7ed6eb5a5aa3
Spir0u <28966599+Spir0u@users.noreply.github.com>
2026-02-22 21:04:24 +0100

    fix: Handle action rejection and server timeout (#1139)
    * add server timeout exception
    
    * forward rejected goal handles
    
    The goal_response_cb should not throw an exception, so the exception is passed in the result and then raised in  send_goal
    
    * return an empty result when result was None due to cancellation/abortion
    
    * fix lint error
    
    ---------
    
    Co-authored-by: Błażej Sowa <bsowa123@gmail.com>

--- rosbridge_library/src/rosbridge_library/internal/actions.py
+++ rosbridge_library/src/rosbridge_library/internal/actions.py
@@ -146,7 +146,7 @@ def args_to_action_goal_instance(inst: ROSMessage, args: list | dict[str, Any] |
 class SendGoal(Generic[ROSActionGoalT, ROSActionResultT, ROSActionFeedbackT]):
     """Helper class to send action goals."""
 
-    result: GetResultServiceResponse[ROSActionResultT] | None = None
+    result: GetResultServiceResponse[ROSActionResultT] | Exception | None = None
 
     def __init__(self, server_timeout_time: float = 1.0, sleep_time: float = 0.001) -> None:
         self.server_timeout_time = server_timeout_time
@@ -166,7 +166,8 @@ class SendGoal(Generic[ROSActionGoalT, ROSActionResultT, ROSActionFeedbackT]):
         assert self.goal_handle is not None
         if not self.goal_handle.accepted:
             msg = "Action goal was rejected"
-            raise Exception(msg)
+            self.result = Exception(msg)
+            return
         result_future: Future[GetResultServiceResponse[ROSActionResultT]] = (
             self.goal_handle.get_result_async()
         )
@@ -195,7 +196,9 @@ class SendGoal(Generic[ROSActionGoalT, ROSActionResultT, ROSActionFeedbackT]):
         client: ActionClient[ROSActionGoalT, ROSActionResultT, ROSActionFeedbackT] = ActionClient(
             node_handle, action_class, action_name
         )
-        client.wait_for_server(timeout_sec=self.server_timeout_time)
+        if not client.wait_for_server(timeout_sec=self.server_timeout_time):
+            msg = "No action server available"
+            raise Exception(msg)
         send_goal_future = client.send_goal_async(inst, feedback_callback=feedback_cb)  # type: ignore[arg-type]
         send_goal_future.add_done_callback(self.goal_response_cb)
 
@@ -203,6 +206,10 @@ class SendGoal(Generic[ROSActionGoalT, ROSActionResultT, ROSActionFeedbackT]):
             time.sleep(self.sleep_time)
 
         client.destroy()
+
+        if isinstance(self.result, Exception):
+            raise self.result
+
         if self.result is not None:
             # Turn the response into JSON and pass to the callback
             json_response = extract_values(self.result)
