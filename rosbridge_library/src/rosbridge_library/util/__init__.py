4acdc99ec49f4fe49c5b8bdf553ef38ab63ade8c
Błażej Sowa <bsowa123@gmail.com>
2026-03-09 20:14:12 +0100

    feat: Drop support for BSON (#1172)

--- rosbridge_library/src/rosbridge_library/util/__init__.py
+++ rosbridge_library/src/rosbridge_library/util/__init__.py
@@ -6,15 +6,3 @@ except ImportError:
         import simplejson as json  # type: ignore[import-untyped, no-redef]
     except ImportError:
         import json  # type: ignore[no-redef] # noqa: F401
-
-import bson
-
-try:
-    _ = bson.BSON
-except AttributeError as exc:
-    msg = (
-        "BSON installation does not support all necessary features. "
-        "Please use the MongoDB BSON implementation. "
-        "See: https://github.com/RobotWebTools/rosbridge_suite/issues/198"
-    )
-    raise Exception(msg) from exc
