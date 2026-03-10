6d6a93aea33df9366dbdcf933804023c072adff7
Michael <michael.swan@fieldai.com>
2026-03-10 13:28:46 +1000

    Fix deadlock issue on init

--- rosbridge_server/scripts/rosbridge_websocket.py
+++ rosbridge_server/scripts/rosbridge_websocket.py
@@ -134,8 +134,6 @@ class RosbridgeWebsocketNode(Node):
         RosbridgeWebSocket.protocol_parameters = self.protocol_parameters
         RosbridgeWebSocket.use_compression = self.use_compression
 
-        self._start_server()
-
     def _handle_parameters(self) -> None:
         # Parse command line arguments
         args = parse_args()
@@ -240,6 +238,9 @@ async def async_main() -> None:
 
     spin_thread = threading.Thread(target=executor.spin)
     spin_thread.start()
+
+    # Start accepting connections once executor is ready
+    node._start_server()
 
     loop = asyncio.get_running_loop()
     stop_event = asyncio.Event()
