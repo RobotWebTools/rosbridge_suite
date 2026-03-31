abe193fd8aea8deb268a2397f6809700ec83a80b
Błażej Sowa <bsowa123@gmail.com>
2026-03-31 21:37:39 +0200

    Fix type hints
    Co-authored-by: Błażej Sowa <bsowa123@gmail.com>

--- rosbridge_library/src/rosbridge_library/internal/subscribers.py
+++ rosbridge_library/src/rosbridge_library/internal/subscribers.py
@@ -172,14 +172,14 @@ class MultiSubscriber(Generic[ROSMessageT]):
         self.new_subscriber: Subscription | None = None
         self.new_subscriptions: dict[str, Callable[[OutgoingMessage[ROSMessageT]], None]] = {}
 
-    def _schedule_destroy_subscription(self, subscription: Subscription[ROSMessageT]) -> None:
+    def _schedule_destroy_subscription(self, subscription: Subscription) -> None:
         """
         Schedule subscription destruction on the executor thread.
 
         Used to avoid race conditions between executor and non-executor threads.
 
         Args:
-            subscription (Subscription[ROSMessageT]): Subscription to destroy
+            subscription (Subscription): Subscription to destroy
 
         """
         executor = self.node_handle.executor
